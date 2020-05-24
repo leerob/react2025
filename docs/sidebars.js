46355ea7ca9dab883f5ab48fa5b99f1c824709c6
Lee Robinson <lrobinson2011@gmail.com>
2020-05-23 23:24:18 -0500

    Update sidebar of docs.

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -6,13 +6,35 @@ module.exports = {
       'getting-started',
       'deploying'
     ],
-    '1: Hello, Firebase': [
+    '1: Hello Firebase': [
       'setting-up-firebase',
       'adding-firebase-to-app',
       'use-auth',
       'saving-user-data',
       'firebase-conclusion'
     ],
+    '2: Leaving Feedback': ['firebase-admin', 'chakra-ui', 'dynamic-routing'],
+    // '3: User Dashboard': [
+    //   'setting-up-firebase',
+    //   'adding-firebase-to-app',
+    //   'use-auth',
+    //   'saving-user-data',
+    //   'firebase-conclusion'
+    // ],
+    // '4: Accepting Payments': [
+    //   'setting-up-firebase',
+    //   'adding-firebase-to-app',
+    //   'use-auth',
+    //   'saving-user-data',
+    //   'firebase-conclusion'
+    // ],
+    // '5: Embeded View': [
+    //   'setting-up-firebase',
+    //   'adding-firebase-to-app',
+    //   'use-auth',
+    //   'saving-user-data',
+    //   'firebase-conclusion'
+    // ],
     'Style Guide': ['style-guide']
   }
 };
