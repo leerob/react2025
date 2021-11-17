87cffc4c0ad8c87a261ce5b65e7e18f7a2882641
Lee Robinson <me@leerob.io>
2021-11-16 22:25:50 -0600

    Upgrade to Next.js 12. (#37)

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,8 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js'
+  themeConfig: './theme.config.js',
+  unstable_stork: true,
+  unstable_staticImage: true
 });
 
 module.exports = withNextra({
---
+++ docs/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './pages/**/*.js',
+    './pages/**/*.md',
+    './pages/**/*.mdx',
+    './theme.config.js',
+    './styles.css'
+  ]
+};
